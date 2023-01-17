0942bbef5f51831ebc36ec3c00c1af6487bf98ca
RAJ KUMAR <103047044+zephyr005@users.noreply.github.com>
2023-01-17 21:58:30 +0530

    Update ImageService.java

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,43 +1,28 @@
 package com.driver.services;
 
 import com.driver.models.*;
-import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
 
-    @Autowired
-    BlogRepository blogRepository;
-
     public Image createAndReturn(Blog blog, String description, String dimensions){
-        //create an image based on given parameters and add it to the imageList of given blog
         Image image = new Image();
         image.setDescription(description);
         image.setDimensions(dimensions);
         image.setBlog(blog);
-
-        List<Image> imageList = blog.getImageList();
-        if(imageList==null) imageList=new ArrayList<>();
-        imageList.add(image);
-
-        blog.setImageList(imageList);
-        blogRepository.save(blog);
-
+        imageRepository2.save(image);
         return image;
-
     }
 
     public void deleteImage(Image image){
+        System.out.println(image.getId());
         imageRepository2.delete(image);
+        System.out.println(imageRepository2.findAll().size());
     }
 
     public Image findById(int id) {
@@ -46,14 +31,35 @@ public class ImageService {
 
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-        //In case the image is null, return 0
-        if (screenDimensions.split("X").length == 2 || Objects.nonNull(image)) {
-            Integer maxLength = Integer.parseInt(screenDimensions.split("X")[0]) / Integer.parseInt(image.getDimensions().split("X")[0]) ;
-            Integer maxBreadth = Integer.parseInt(screenDimensions.split("X")[1]) / Integer.parseInt(image.getDimensions().split("X")[1]);
-            return maxLength * maxBreadth;
+        String dimensions = image.getDimensions();
+        int xi = 0;
+        int yi = 0;
+        int xs = 0;
+        int ys = 0;
+        int num = 0;
+        for(int i = 0; i<dimensions.length(); i++){
+            if(dimensions.charAt(i) == 'X'){
+                xi = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (dimensions.charAt(i) - '0');
         }
-        return 0;
+        yi = num;
+        num = 0;
+        for(int i = 0; i<screenDimensions.length(); i++){
+            if(screenDimensions.charAt(i) == 'X'){
+                xs = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (screenDimensions.charAt(i) - '0');
+        }
+        ys = num;
 
-
+        int ans = (int) (Math.floor((new Double(xs))/(new Double(xi))) * Math.floor((new Double(ys))/(new Double(yi))));
+        return ans;
     }
 }
