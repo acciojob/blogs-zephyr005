eb27be312ce9cb64813731f2aab46d763a5f02fc
RAJ KUMAR <103047044+zephyr005@users.noreply.github.com>
2023-01-17 21:53:44 +0530

    Update Blog.java

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -1,37 +1,45 @@
 package com.driver.models;
 
-import org.hibernate.annotations.CreationTimestamp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "blog")
+@Table(name = "Blog")
 public class Blog {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String title;
-
     private String content;
-
-
     private Date pubDate;
 
-    public Blog(){
-
+    public User getUser() {
+        return user;
     }
 
-    public Blog(int id ,String title,String content, Date date,List<Image> imageList){
-        this.id = id;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<Image> imageList) {
         this.imageList = imageList;
-        this.title = title;
-        this.content = content;
-        this.pubDate = date;
     }
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @OneToMany(mappedBy = "blog", cascade = CascadeType.ALL)
+    private List<Image> imageList;
 
     public int getId() {
         return id;
@@ -60,33 +68,7 @@ public class Blog {
     public Date getPubDate() {
         return pubDate;
     }
-
     public void setPubDate(Date pubDate) {
         this.pubDate = pubDate;
-    }
-
-    //    as blog and user have parent child relation M to One
-    @ManyToOne
-    @JoinColumn
-    private User user;
-
-    // blog is parent of image with one to many relation
-    @OneToMany(mappedBy = "blog",cascade = CascadeType.ALL)
-    private List<Image> imageList;
-
-    public List<Image> getImageList() {
-        return imageList;
-    }
-
-    public void setImageList(List<Image> imageList) {
-        this.imageList = imageList;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 }
