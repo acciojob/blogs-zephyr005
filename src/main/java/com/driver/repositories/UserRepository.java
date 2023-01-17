99478a5ad17615eb8cf942061b692a3d48615d64
RAJ KUMAR <103047044+zephyr005@users.noreply.github.com>
2023-01-17 21:59:58 +0530

    Update UserRepository.java

--- src/main/java/com/driver/repositories/UserRepository.java
+++ src/main/java/com/driver/repositories/UserRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
 }
